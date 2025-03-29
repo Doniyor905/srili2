3e293f4521d6c75e59224b79fdf0bd5471b3a3cd
doniyor <usupovd276@gmail.com>
2025-03-29 15:07:50 +0300

    addd

--- app/api/about/route.ts
+++ app/api/about/route.ts
@@ -2,6 +2,8 @@ import { prisma } from '@/prisma/prisma-client';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const about = await prisma.about.findMany();
+  const about = await prisma.about.findMany({
+    orderBy: { id: 'asc' },
+  });
   return NextResponse.json(about);
 }
